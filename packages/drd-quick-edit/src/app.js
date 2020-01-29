eed5fe9b3017eb4f38a56f8de521d96646060be8
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-01-29 15:26:28 +0100

    feat(drd-quick-edit): mock interactions

--- packages/drd-quick-edit/src/app.js
+++ packages/drd-quick-edit/src/app.js
@@ -4,15 +4,41 @@ import $ from 'jquery';
 
 import diagram from '../resources/diagram.svg';
 
+
+function openEditModal() {
+  
+  const decision = $(this);
+
+  // mark as selected
+  decision.addClass('selected');
+
+}
+
+function mockInteractions(decision) {
+
+  const hitBox = decision.children('.djs-hit');
+
+  const outline = decision.children('.djs-outline');
+
+  decision.mouseover(() => decision.addClass('hover'));
+
+  decision.mouseout(() => decision.removeClass('hover'))
+}
+
 function main() {
   
   const diagramGfx = $(diagram)[0];
 
   const contents = $('.contents')[0];
 
+  // insert diagram svg into page
   contents.append(diagramGfx);
 
-  debugger;
+  const decision = $('.demo-decision');
+
+  mockInteractions(decision);
+
+  decision.click(openEditModal);
 
 }
 
