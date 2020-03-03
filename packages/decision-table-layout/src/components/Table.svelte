4fdb5bae901ea8c616e06876aa80b5f5072fc1fb
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-03-03 11:27:40 +0100

    chore(decision-table-layout): encapsulate decision table data as props

--- packages/decision-table-layout/src/components/Table.svelte
+++ packages/decision-table-layout/src/components/Table.svelte
@@ -1,159 +1,159 @@
 <script>
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
-  import { find, forEach } from 'min-dash';
+  import { find, forEach } from "min-dash";
 
-  import dom from 'domtastic';
+  import dom from "domtastic";
 
-  import './Table.scss';
+  import "./Table.scss";
 
   const HIT_POLICIES = [
     {
-      name: 'Unique',
+      name: "Unique",
       explanation:
-        'No overlap is possible and all rules are disjoint. Only a single rule can be matched'
+        "No overlap is possible and all rules are disjoint. Only a single rule can be matched"
     },
     {
-      name: 'Any',
-      explanation: 'Rules may overlap. Their output have to match'
+      name: "Any",
+      explanation: "Rules may overlap. Their output have to match"
     },
     {
-      name: 'Priority',
+      name: "Priority",
       explanation:
-        'Rules may overlap. The one with the highest priority will be chosen'
+        "Rules may overlap. The one with the highest priority will be chosen"
     },
     {
-      name: 'First',
-      explanation: 'Rules may overlap. The first matching rule will be chosen'
+      name: "First",
+      explanation: "Rules may overlap. The first matching rule will be chosen"
     },
     {
-      name: 'Collect',
-      explanation: 'Collects the values of all matching rules'
+      name: "Collect",
+      explanation: "Collects the values of all matching rules"
     },
     {
-      name: 'Collect (Sum)',
+      name: "Collect (Sum)",
       explanation:
-        'Collects the values of all matching rules and sums up to a single value'
+        "Collects the values of all matching rules and sums up to a single value"
     },
     {
-      name: 'Collect (Max)',
+      name: "Collect (Max)",
       explanation:
-        'Collects the values of all matching rules and uses the highest value'
+        "Collects the values of all matching rules and uses the highest value"
     },
     {
-      name: 'Collect (Min)',
+      name: "Collect (Min)",
       explanation:
-        'Collects the values of all matching rules and uses the lowest value'
+        "Collects the values of all matching rules and uses the lowest value"
     },
     {
-      name: 'Collect (Count)',
+      name: "Collect (Count)",
       explanation:
-        'Collects the values of all matching rules and counts the number of them'
+        "Collects the values of all matching rules and counts the number of them"
     }
   ];
 
-  const inputHeaders = [
-    {
-      idx: 0,
-      clause: 'When',
-      name: 'Open Claims',
-      type: 'integer'
-    },
-    {
-      idx: 1,
-      clause: 'And',
-      name: 'Employee.region = Claim.region',
-      type: '[true, false]'
-    },
-    {
-      idx: 2,
-      clause: 'And',
-      name: 'Claim.expenditure',
-      type: 'integer'
-    },
-    {
-      idx: 3,
-      clause: 'And',
-      name: 'Employee Experience',
-      type: "['Junior', 'Senior', 'Experienced']",
-      smaller: true
-    },
-    {
-      idx: 4,
-      clause: 'And',
-      name: 'Employee fills skillset',
-      type: '[true, false]'
-    }
-  ];
+  export let tableData = {
+    inputHeaders: [
+      {
+        idx: 0,
+        clause: "When",
+        name: "Open Claims",
+        type: "integer"
+      },
+      {
+        idx: 1,
+        clause: "And",
+        name: "Employee.region = Claim.region",
+        type: "[true, false]"
+      },
+      {
+        idx: 2,
+        clause: "And",
+        name: "Claim.expenditure",
+        type: "integer"
+      },
+      {
+        idx: 3,
+        clause: "And",
+        name: "Employee Experience",
+        type: "['Junior', 'Senior', 'Experienced']",
+        smaller: true
+      },
+      {
+        idx: 4,
+        clause: "And",
+        name: "Employee fills skillset",
+        type: "[true, false]"
+      }
+    ],
+    outputHeaders: [
+      {
+        idx: 0,
+        clause: "Then",
+        name: "Score",
+        type: "[> -50; < 20]"
+      }
+    ],
+    rules: [
+      {
+        idx: 1,
+        inputCells: ["<=5", "-", "-", "-", "-"],
+        outputCells: ["10"]
+      },
+      {
+        idx: 2,
+        inputCells: [">10", "-", "-", "'Junior'", "-"],
+        outputCells: ["-25"]
+      },
+      {
+        idx: 3,
+        inputCells: [">5", "-", "-", "'Experienced', 'Senior'", "-"],
+        outputCells: ["-10"]
+      },
+      {
+        idx: 4,
+        inputCells: ["-", "true", "-", "-", "-"],
+        outputCells: ["20"]
+      },
+      {
+        idx: 5,
+        inputCells: ["-", "-", "<1000", "'Junior', 'Experienced'", "-"],
+        outputCells: ["0"]
+      },
+      {
+        idx: 6,
+        inputCells: ["-", "-", "<1000", "'Senior'", "-"],
+        outputCells: ["-20"]
+      },
+      {
+        idx: 7,
+        inputCells: ["-", "-", "[1000..10000]", "'Junior'", "-"],
+        outputCells: ["-15"]
+      },
+      {
+        idx: 8,
+        inputCells: ["-", "-", ">10000", "'Junior'", "-"],
+        outputCells: ["-50"]
+      },
+      {
+        idx: 9,
+        inputCells: ["-", "-", ">10000", "'Experienced'", "-"],
+        outputCells: ["-15"]
+      },
+      {
+        idx: 10,
+        inputCells: ["-", "-", "-", "'Junior'", "false"],
+        outputCells: ["-50"]
+      },
+      {
+        idx: 11,
+        inputCells: ["-", "-", "-", "'Experienced', 'Senior'", "false"],
+        outputCells: ["-10"]
+      }
+    ]
+  };
 
-  const outputHeaders = [
-    {
-      idx: 0,
-      clause: 'Then',
-      name: 'Score',
-      type: '[> -50; < 20]'
-    }
-  ];
-
-  const rules = [
-    {
-      idx: 1,
-      inputCells: ['<=5', '-', '-', '-', '-'],
-      outputCells: ['10']
-    },
-    {
-      idx: 2,
-      inputCells: ['>10', '-', '-', "'Junior'", '-'],
-      outputCells: ['-25']
-    },
-    {
-      idx: 3,
-      inputCells: ['>5', '-', '-', "'Experienced', 'Senior'", '-'],
-      outputCells: ['-10']
-    },
-    {
-      idx: 4,
-      inputCells: ['-', 'true', '-', '-', '-'],
-      outputCells: ['20']
-    },
-    {
-      idx: 5,
-      inputCells: ['-', '-', '<1000', "'Junior', 'Experienced'", '-'],
-      outputCells: ['0']
-    },
-    {
-      idx: 6,
-      inputCells: ['-', '-', '<1000', "'Senior'", '-'],
-      outputCells: ['-20']
-    },
-    {
-      idx: 7,
-      inputCells: ['-', '-', '[1000..10000]', "'Junior'", '-'],
-      outputCells: ['-15']
-    },
-    {
-      idx: 8,
-      inputCells: ['-', '-', '>10000', "'Junior'", '-'],
-      outputCells: ['-50']
-    },
-    {
-      idx: 9,
-      inputCells: ['-', '-', '>10000', "'Experienced'", '-'],
-      outputCells: ['-15']
-    },
-    {
-      idx: 10,
-      inputCells: ['-', '-', '-', "'Junior'", 'false'],
-      outputCells: ['-50']
-    },
-    {
-      idx: 11,
-      inputCells: ['-', '-', '-', "'Experienced', 'Senior'", 'false'],
-      outputCells: ['-10']
-    }
-  ];
-
-  let hitPolicy = 'Collect (Sum)';
+  let hitPolicy = "Collect (Sum)";
   $: explanation = find(HIT_POLICIES, hp => hp.name === hitPolicy).explanation;
 
   function changeHitPolicy(event) {
@@ -165,21 +165,20 @@
   }
 
   function initHeaderInteractions(header) {
-  
     // do not do anything if not in split screen
     if (onHighlight === noop) {
       return;
     }
 
-    header.addClass('table-header');
+    header.addClass("table-header");
 
-    header.on('mouseover', () => {
-      header.addClass('hover');
+    header.on("mouseover", () => {
+      header.addClass("hover");
       onHighlight(header);
     });
 
-    header.on('mouseout', () => {
-      header.removeClass('hover');
+    header.on("mouseout", () => {
+      header.removeClass("hover");
       onHighlight(header);
     });
   }
@@ -189,6 +188,11 @@
   export let onHighlight = noop;
 
   onMount(() => {
+    const {
+      inputHeaders,
+      outputHeaders
+    } = tableData;
+
     forEach(inputHeaders, ({ idx }) => {
       const header = dom(`[data-header-id="input-header-${idx}"`);
       initHeaderInteractions(header);
@@ -220,7 +224,7 @@
       <tr class="header-row">
         <th />
 
-        {#each inputHeaders as { idx, clause, name, type, smaller }}
+        {#each tableData.inputHeaders as { idx, clause, name, type, smaller }}
           <th class="input-header" data-header-id={'input-header-' + idx}>
             <span class="clause">{clause}</span>
             <p class="label">{name}</p>
@@ -230,7 +234,7 @@
           </th>
         {/each}
 
-        {#each outputHeaders as { idx, clause, name, type }}
+        {#each tableData.outputHeaders as { idx, clause, name, type }}
           <th
             class="output-header output-cell"
             data-header-id={'output-header-' + idx}>
@@ -245,7 +249,7 @@
     </thead>
     <tbody>
 
-      {#each rules as { idx, inputCells, outputCells }}
+      {#each tableData.rules as { idx, inputCells, outputCells }}
         <tr id={idx}>
           <td class="rule">{idx}</td>
           {#each inputCells as input}
