88b4920d365878780fb08512b13fcc8d6426e923
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-04-01 14:34:37 +0200

    feat(variant-c): add resize handlers

--- packages/column-editing/src/components/variant_C/CenteredAlignWrapper.svelte
+++ packages/column-editing/src/components/variant_C/CenteredAlignWrapper.svelte
@@ -1,7 +1,18 @@
 <script>
+    import ResizableColumnWrapper from '../shared/ResizableColumnWrapper';
+
     import './CenteredAlignWrapper.scss';
+
+    const noop = () => {};
+    
+    // exports //////////
+
+    export let onResizeEnd = noop;
+    export let onResizeStart = noop;
 </script>
 
 <div class="centered-align-wrapper">
-  <slot/>
+    <ResizableColumnWrapper {onResizeStart} {onResizeEnd}>
+        <slot/>
+    </ResizableColumnWrapper>
 </div>
